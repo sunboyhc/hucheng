64c4e02f71357eac4112246188c405325c5bbe7d
hu cheng <hucheng88@gmail.com>
2013-04-25 20:16:24 +0800

    first commit

---
+++ test/test/test.cpp
@@ -0,0 +1,13 @@
+// test.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include "stdlib.h"
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	printf("hello world!\n");
+	system("Pause");
+	return 0;
+}
+
